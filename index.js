65080d77b1da8b93912b2b92d400627fff88421a
martin mithi <gathitumithi9000@gmail.com>
2022-08-26 22:05:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const comp = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (comp === array[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
-/* 
+/*                 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  -for(elements(i) in the array){
+    let comp=target-array[i]}
+  -for(rest of the elements(j) in the array){
+      if(array[j]===comp){
+        return true
+      }
+      else{
+        return false
+      }
+  }
+ 
 */
 
 /*
   Add written explanation of your solution here
+  -We are looping through an array looking for 2 pairs of numbers that add up to target
+  -We can get the complement of the current element(array[i]) by doing target-array[i]
+  -We then look for the complement through the rest of the array and if found we return true; else return false
 */
 
 // You can run `node index.js` to view these console logs
